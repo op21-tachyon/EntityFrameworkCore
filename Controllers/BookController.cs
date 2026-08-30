d77c3fd79dfb69feb06a75f6804d457d7b7134f7
rakesh <rakeshiiitald@gmail.com>
2026-08-30 12:52:37 +0530

    BookController

---
+++ Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BookController(AppDbContext appContext)
+        {
+            _appDbContext = appContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var result = (from books in _appDbContext.Books
+                          select books).ToList();
+            return Ok(result);
+        }
+    }
+}
